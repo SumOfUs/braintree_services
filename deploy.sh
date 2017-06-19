0bdf1baea3bd21a63a72050137c4332431286534
Omar Sahyoun <osahyoun@gmail.com>
2017-06-19 14:27:09 +0100

    Ditch yarn - doesn't seem to work with serverless

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 serverless deploy -s prod \
   --verbose \
-  --merchant_id ${MERCHANT_ID} \
-  --public_key ${PUBLIC_KEY} \
-  --private_key ${PRIVATE_KEY} \
+  --merchant_id $MERCHANT_ID \
+  --public_key $PUBLIC_KEY \
+  --private_key $PRIVATE_KEY \
   --env Production
